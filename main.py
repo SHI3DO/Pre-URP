c1bf253a065139e513def8e4428614ac0ede545c
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-08-02 16:47:31 +0900

    d

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import sys
+import time
 
 import numpy as np
 import logging
 import mne
 import warnings
+
 
 warnings.simplefilter("ignore", DeprecationWarning)
 logging.basicConfig(format="%(asctime)s - %(levelname)s — %(funcName)s:%(lineno)d ——— %(message)s",
@@ -13,6 +15,8 @@ logging.basicConfig(format="%(asctime)s - %(levelname)s — %(funcName)s:%(linen
                     handlers=[logging.FileHandler("logs.txt", 'w', encoding='UTF-8')])
 
 logger = logging.getLogger()
+
+
 def load_file(filename):
     return os.path.join(os.path.dirname(__file__), filename)
 
@@ -27,7 +31,11 @@ print(gdf_files)
 # noinspection PyTypeChecker
 for file in gdf_files:
     raw = mne.io.read_raw_gdf(file)
-    print(raw.info)
-    print(raw.ch_names)
-    logger.info(file)
-    logger.info(raw.info)
+    raw_info = raw.info
+    raw_ch_names = raw.ch_names
+    print(raw_info, raw_ch_names)
+    event = mne.events_from_annotations(raw)
+    epoch = mne.Epochs(raw, event[0], event_repeated="merge", preload=True)
+    print(epoch.info)
+    logger.info(raw_info);logger.info(raw_ch_names);logger.info(epoch.info)
+
